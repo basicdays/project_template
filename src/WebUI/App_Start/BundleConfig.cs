8c8977a1948cc45b51f6af296515471b887fb77b
basicdays <paul@basicdays.com>
2013-05-26 21:16:34 -0500

    Add bootstrap scripts, remove knockout

--- src/WebUI/App_Start/BundleConfig.cs
+++ src/WebUI/App_Start/BundleConfig.cs
@@ -7,22 +7,31 @@ namespace BuildHealth2013.WebUI.App_Start
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/main.css"));
+            // Can comment parts that are not in use
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"
+                        //"~/Scripts/bootstrap-transition",
+                        //"~/Scripts/bootstrap-affix",
+                        //"~/Scripts/bootstrap-alert",
+                        //"~/Scripts/bootstrap-button",
+                        //"~/Scripts/bootstrap-carousel",
+                        //"~/Scripts/bootstrap-collapse",
+                        //"~/Scripts/bootstrap-dropdown",
+                        //"~/Scripts/bootstrap-modal",
+                        //"~/Scripts/bootstrap-scrollspy",
+                        //"~/Scripts/bootstrap-tab",
+                        //"~/Scripts/bootstrap-tooltip",
+                        //"~/Scripts/bootstrap-popover",
+                        //"~/Scripts/bootstrap-typeahead"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
@@ -37,6 +46,8 @@ namespace BuildHealth2013.WebUI.App_Start
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/main.css"));
         }
     }
 }
